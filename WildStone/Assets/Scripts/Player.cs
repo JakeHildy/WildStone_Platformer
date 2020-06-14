de0d4efb010febae445e918fea886619f3cff248
jacobHildebrandt <jake@hildy.ca>
2020-06-14 15:43:01 -0700

    Flip Sprite on Movement

--- WildStone/Assets/Scripts/Player.cs
+++ WildStone/Assets/Scripts/Player.cs
@@ -18,12 +18,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
     {
-        float translation = Input.GetAxis("Horizontal"); // -1 to +1
-        Vector2 playerVelocity = new Vector2(translation * speed, myRigidbody.velocity.y);
+        float controlThrow = Input.GetAxis("Horizontal"); // -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * speed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
     }
 }
