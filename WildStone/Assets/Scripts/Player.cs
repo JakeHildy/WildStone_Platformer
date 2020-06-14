aafb3547b84ffc107c67266fbb468d459cc56b8b
jacobHildebrandt <jake@hildy.ca>
2020-06-14 15:30:09 -0700

    Added Horizontal Movement

---
+++ WildStone/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float speed = 10.0f;
+    Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float translation = Input.GetAxis("Horizontal"); // -1 to +1
+        Vector2 playerVelocity = new Vector2(translation * speed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+}
