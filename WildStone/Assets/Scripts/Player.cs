13b38d07cee96e83b399928cc43cd9682f49051e
jacobHildebrandt <jake@hildy.ca>
2020-06-14 18:53:12 -0700

    Implemented Jumping

--- WildStone/Assets/Scripts/Player.cs
+++ WildStone/Assets/Scripts/Player.cs
@@ -6,12 +6,14 @@ public class Player : MonoBehaviour
 {
     // Config
     [SerializeField] float speed = 10.0f;
+    [SerializeField] float jumpForce = 5.0f;
 
     // State
     bool isAlive = true;
 
     // Cached component references
     Rigidbody2D myRigidbody;
+    Collider2D myCollider;
     Animator myAnimator;
 
     
@@ -19,6 +21,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
         myAnimator = GetComponent<Animator>();
     }
 
@@ -27,6 +30,7 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Jump();
     }
 
     private void Run()
@@ -35,8 +39,18 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlThrow * speed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
 
-        print(myRigidbody.velocity);
         myAnimator.SetBool("Running", IsRunning());
+    }
+
+    private void Jump()
+    {
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {return;}
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpForce);
+            myRigidbody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
