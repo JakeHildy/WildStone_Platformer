0b5c66a50c31c32108179a796ad18b2e1d2b38d9
jacobHildebrandt <jake@hildy.ca>
2020-06-20 09:09:27 -0700

    Added 3 level and transition between Scenes

---
+++ WildStone/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float sceneDelayTime = 2.0f;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if ((other as CapsuleCollider2D) && (other.gameObject.tag == "Player"))
+        {            
+            StartCoroutine(LoadNextScene());
+        }
+        
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(sceneDelayTime);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
--- WildStone/Assets/Scripts/Player.cs
+++ WildStone/Assets/Scripts/Player.cs
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
         {
             myAnimator.SetTrigger("Die");
             myRigidbody.velocity = deathKick;
